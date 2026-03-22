c5e4eafe7de825fc277d725a262296bf7f0f718c
Kris Walker <kris@kixx.name>
2026-03-22 11:58:25 -0400

    Update local configs to make app deployable
    The way it was configured would override our remote configurations
    
     Changes to be committed:
    	modified:   vitest.config.js
    	modified:   wrangler.jsonc

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,13 @@ export default defineWorkersConfig({
 		poolOptions: {
 			workers: {
 				wrangler: { configPath: './wrangler.jsonc' },
+				miniflare: {
+					bindings: {
+						GITHUB_CLIENT_ID: 'test-client-id',
+						GITHUB_CLIENT_SECRET: 'test-client-secret',
+						ALLOWED_EMAILS: 'allowed@example.com',
+					},
+				},
 			},
 		},
 	},
